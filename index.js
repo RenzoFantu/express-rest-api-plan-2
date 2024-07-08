98fb8a5683fd7466709cd3c5d85b88fb2a228831
RenzoFantu <rfantuzzi@gmail.com>
2024-07-07 21:23:53 -0400

    agregue cors

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./src/routes/index');
 const app = express();
-
+const cors = require('cors')
 const port = process.env.PORT || 3000;
 
+const corsOptions = {
+    origin: 'http://localhost:5173', // Permitir solicitudes desde este origen
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: ['Content-Type', 'Authorization']
+  };
+  
+  app.use(cors(corsOptions));
 
 app.use(express.json());
 
